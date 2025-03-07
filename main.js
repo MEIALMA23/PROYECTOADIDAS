644ae07b9d5e8fbe6cde73c56b3814e6a4fe8ae3
MEIALMA23 <almazan.smmr@gmail.com>
2025-03-07 11:34:55 -0300

    CAMBIOS

---
+++ main.js
@@ -0,0 +1,39 @@
+// const obj ={
+//     nombre: 'nombre',
+//     apellido: 'apellido',
+//     edad: 'edad',
+// }
+
+const productosRemeras =[
+    {
+        nombre: 'Remera 1',
+        precio: 150,
+        color: 'verde',
+    },
+    {
+        nombre: 'Remera 2',
+        precio: 200,
+        color: 'roja',
+    },
+    {
+        nombre: 'Remera 3',
+        precio: 200,
+        color: 'amarilla',
+    },
+    {
+        nombre: 'Remera 4',
+        precio: 200,
+        color: 'azul',
+    },
+    {
+        nombre: 'Remera 5',
+        precio: 200,
+        color: 'rosa',
+    },
+
+]
+
+//contenedor
+const container = document.querySelector('.container');
+
+
